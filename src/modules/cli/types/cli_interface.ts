1fbdd953a20f0d7ae997d265d85595a1ab040080
ArtificialLegacy <redsbro457@gmail.com>
2023-09-09 16:05:52 -0400

    interface type docs

--- src/modules/cli/types/cli_interface.ts
+++ src/modules/cli/types/cli_interface.ts
@@ -1,7 +1,9 @@
 import type readline from 'readline'
 
+// The type structure for the interface wrapper class
 type CliInstance = {}
 
+// The type of the cli interface library used.
 type CliInterface = readline.Interface
 
 export type { CliInstance, CliInterface }
