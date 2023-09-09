823fb4a6db62ffb621f76215c0204c51eeef8879
ArtificialLegacy <redsbro457@gmail.com>
2023-09-09 17:35:14 -0400

    added reset ansi code

--- src/modules/cli/constants/cli_color.ts
+++ src/modules/cli/constants/cli_color.ts
@@ -1,4 +1,6 @@
 enum CliColor {
+  Reset = '\u001b[0m',
+
   Black = '\u001b[30m',
   Red = '\u001b[31m',
   Green = '\u001b[32m',
@@ -7,6 +9,7 @@ enum CliColor {
   Magenta = '\u001b[35m',
   Cyan = '\u001b[36m',
   White = '\u001b[37m',
+
   BlackBackground = '\u001b[40m',
   RedBackground = '\u001b[41m',
   GreenBackground = '\u001b[42m',
