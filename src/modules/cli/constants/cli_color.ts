9769ddc1030d5964acaeb669aca1b4f6c2b1a11a
ArtificialLegacy <redsbro457@gmail.com>
2023-09-09 16:51:38 -0400

    add enum constant for ansi colors

---
+++ src/modules/cli/constants/cli_color.ts
@@ -0,0 +1,20 @@
+enum CliColor {
+  Black = '\u001b[30m',
+  Red = '\u001b[31m',
+  Green = '\u001b[32m',
+  Yellow = '\u001b[33m',
+  Blue = '\u001b[34m',
+  Magenta = '\u001b[35m',
+  Cyan = '\u001b[36m',
+  White = '\u001b[37m',
+  BlackBackground = '\u001b[40m',
+  RedBackground = '\u001b[41m',
+  GreenBackground = '\u001b[42m',
+  YellowBackground = '\u001b[43m',
+  BlueBackground = '\u001b[44m',
+  MagentaBackground = '\u001b[45m',
+  CyanBackground = '\u001b[46m',
+  WhiteBackground = '\u001b[47m',
+}
+
+export default CliColor
