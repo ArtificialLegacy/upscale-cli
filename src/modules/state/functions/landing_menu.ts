b42523d99e866e06cd2cc79648e08738db1fed41
ArtificialLegacy <redsbro457@gmail.com>
2023-09-13 17:44:14 -0400

    add cli menu to the landing menu

--- src/modules/state/functions/landing_menu.ts
+++ src/modules/state/functions/landing_menu.ts
@@ -4,8 +4,15 @@ import { State } from 'modules/state_machine'
  * Program state for the landing menu.
  * The main menu that the user sees when they run the program.
  */
-const landing_menu = new State((_, cli) => {
-  cli.print('test')
+const landing_menu = new State(async (_, cli) => {
+  const response = await cli.menu('Select task to perform:', [
+    'Run Workload',
+    'Manage Real-ESRGAN',
+    'Exit',
+  ])
+
+  cli.clear()
+  cli.print(response)
 })
 
 export default landing_menu
