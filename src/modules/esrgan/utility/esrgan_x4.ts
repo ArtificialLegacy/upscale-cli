60957a97d083315cc886db1582392baccc64c883
ArtificialLegacy <redsbro457@gmail.com>
2023-09-25 01:56:14 -0400

    esrgan_x4 utility docs

--- src/modules/esrgan/utility/esrgan_x4.ts
+++ src/modules/esrgan/utility/esrgan_x4.ts
@@ -1,6 +1,11 @@
 import type { CliInstance } from 'modules/cli'
 import { command_run } from 'modules/cmd'
 
+/**
+ * Runs RealESRGAN-x4plus model on the given image.
+ * @param infile - The input file.
+ * @param cli - The CLI instance.
+ */
 async function esrgan_x4(infile: string, cli: CliInstance) {
   cli.clear()
 
