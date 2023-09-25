59d78b51cb188481207351c807110efe8ce47219
ArtificialLegacy <redsbro457@gmail.com>
2023-09-24 20:57:48 -0400

    update esrgan_remove

--- src/modules/esrgan/utility/esrgan_remove.ts
+++ src/modules/esrgan/utility/esrgan_remove.ts
@@ -3,8 +3,19 @@ import fs from 'fs'
 /**
  * Removes the esrgan-tool folder.
  */
-function esrgan_remove() {
-  fs.unlink(`${global.__basedir}\\..\\esrgan-tool`, () => {})
+async function esrgan_remove(): Promise<null> {
+  const promise = new Promise<null>((resolve, reject) => {
+    fs.rm(
+      `${global.__basedir}\\..\\esrgan-tool\\`,
+      { recursive: true },
+      (err) => {
+        if (err) reject(err)
+        else resolve(null)
+      },
+    )
+  })
+
+  return promise
 }
 
 export default esrgan_remove
