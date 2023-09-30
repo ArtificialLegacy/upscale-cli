a0fd49369a0e0ecf5b3f537a67d8b0f7e48328c7
ArtificialLegacy <redsbro457@gmail.com>
2023-09-29 22:16:10 -0400

    utility function docs

--- src/utility/directory_exists.ts
+++ src/utility/directory_exists.ts
@@ -1,5 +1,10 @@
 import fs from 'fs/promises'
 
+/**
+ * Check if a directory exists.
+ * @param filepath - The path to the directory to check.
+ * @returns A promise that resolves to true if the directory exists, false otherwise.
+ */
 async function directory_exists(filepath: string): Promise<boolean> {
   const dirExists = await fs
     .stat(filepath)
--- src/utility/file_exists.ts
+++ src/utility/file_exists.ts
@@ -1,5 +1,10 @@
 import fs from 'fs/promises'
 
+/**
+ * Check if a file exists.
+ * @param filepath - The path to the file to check.
+ * @returns A promise that resolves to true if the file exists, false otherwise.
+ */
 async function file_exists(filepath: string): Promise<boolean> {
   const fileExists = await fs
     .stat(filepath)
