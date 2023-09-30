a1d4593aecb75003cc0440605983e6a9de34b2d4
ArtificialLegacy <redsbro457@gmail.com>
2023-09-29 22:55:07 -0400

    add export for workload_begin

--- src/modules/esrgan/index.ts
+++ src/modules/esrgan/index.ts
@@ -1,3 +1,4 @@
 export { default as esrgan_verify } from './utility/esrgan_verify'
 export { default as esrgan_remove } from './utility/esrgan_remove'
 export { default as esrgan_x4 } from './utility/esrgan_x4'
+export { default as workload_begin } from './utility/workload_begin'
