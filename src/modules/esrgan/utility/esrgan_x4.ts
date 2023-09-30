cbdfc74d28245d739a7e71a43acdc5dd77fe6da8
ArtificialLegacy <redsbro457@gmail.com>
2023-09-29 21:52:59 -0400

    finish esrgan_x4 workload function

--- src/modules/esrgan/utility/esrgan_x4.ts
+++ src/modules/esrgan/utility/esrgan_x4.ts
@@ -1,7 +1,7 @@
 import fs from 'fs/promises'
 
 import { command_run } from 'modules/cmd'
-import { CliControl } from 'modules/cli'
+import { CliControl, CliColor } from 'modules/cli'
 import directory_exists from 'utility/directory_exists'
 
 /**
@@ -13,14 +13,16 @@ async function esrgan_x4(infile: string) {
   CliControl.clear()
 
   const filename = infile.split('\\').pop()
-  CliControl.print(`Running RealESRGAN-x4plus on ${filename}`)
+  CliControl.print(
+    `${CliColor.Cyan}!${CliColor.Reset} Running RealESRGAN-x4plus on ${filename}\n`,
+  )
 
   if (!(await directory_exists(`${global.__basedir}\\..\\outputs`))) {
     await fs.mkdir(`${global.__basedir}\\..\\outputs`)
   }
 
   await command_run(
-    `${global.__basedir}\\..\\esrgan-tool\\realesrgan-ncnn-vulkan.exe -i ${infile} -o ${global.__basedir}\\outputs\\up_${filename} -n realesr-x4plus`,
+    `${global.__basedir}\\..\\esrgan-tool\\realesrgan-ncnn-vulkan.exe -i ${infile} -o ${global.__basedir}\\..\\outputs\\up_${filename} -n realesrgan-x4plus`,
   )
 }
 
