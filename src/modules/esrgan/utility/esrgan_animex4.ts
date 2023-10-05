7dd42abffea8db74e179b14046585d57635e368d
ArtificialLegacy <redsbro457@gmail.com>
2023-10-05 19:46:03 -0400

    Add support for batching upscales within a directory esrgan_x4 anime

--- src/modules/esrgan/utility/esrgan_animex4.ts
+++ src/modules/esrgan/utility/esrgan_animex4.ts
@@ -1,8 +1,19 @@
 import { command_run } from 'modules/cmd'
 import workload_init from './workload_init'
 
-async function esrgan_animex4(infile: string) {
-  const filename = await workload_init(infile, 'RealESRGAN-x4plus Anime')
+/**
+ * Runs RealESRGAN-x4plus-Anime model on the given image.
+ * @param infile - The input file.
+ * @param index - The index of the input file in a batch workload.
+ * @param total - The total number of input files in a batch workload.
+ */
+async function esrgan_animex4(infile: string, index?: number, total?: number) {
+  const filename = await workload_init(
+    infile,
+    'RealESRGAN-x4plus Anime',
+    index,
+    total,
+  )
 
   await command_run(
     `${global.__basedir}\\..\\esrgan-tool\\realesrgan-ncnn-vulkan.exe -i ${infile} -o ${global.__basedir}\\..\\outputs\\up_${filename} -n realesrgan-x4plus-anime`,
--- src/modules/state/functions/esrgan_animex4.ts
+++ src/modules/state/functions/esrgan_animex4.ts
@@ -2,12 +2,27 @@ import { State } from 'modules/state_machine'
 import type { StateOnFunction } from 'modules/state_machine'
 import { esrgan_animex4, workload_begin } from 'modules/esrgan'
 import { CliControl } from 'modules/cli'
+import file_exists from 'utility/file_exists'
+import directory_exists from 'utility/directory_exists'
+import directory_files from 'utility/directory_files'
 
 const esrgan_animex4_on: StateOnFunction = async (_, transition) => {
   CliControl.clear()
 
   const answer = await workload_begin()
-  await esrgan_animex4(answer)
+
+  const file = await file_exists(answer)
+  const dir = await directory_exists(answer)
+
+  if (file) await esrgan_animex4(answer)
+
+  if (dir) {
+    const files = await directory_files(answer)
+
+    for (const [index, file] of files.entries()) {
+      await esrgan_animex4(`${answer}\\${file}`, index, files.length)
+    }
+  }
 
   return transition('workload_finish')
 }
