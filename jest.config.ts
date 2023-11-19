e1fc52fba179ce0406dd698fcb2398aae676aa37
ArtificialLegacy <redsbro457@gmail.com>
2023-11-19 04:01:25 -0500

    allow jest to work with ts paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from 'jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
 
 const config: Config = {
   verbose: true,
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  modulePaths: ['<rootDir>/src/'],
 }
 
 export default config
