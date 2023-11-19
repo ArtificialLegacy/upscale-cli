f4e1df997b1851fc4103f326527c68a7b979c1ff
ArtificialLegacy <redsbro457@gmail.com>
2023-11-18 19:31:52 -0500

    remove unneeded async IIFE

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,8 @@ denv()
 
 import { state_init } from 'modules/state'
 
-// used instead of top level await due to oddities with ts-node.
-;(async () => {
-  const state = state_init()
-
-  state.transition('esrgan_verify')
-})()
+const state = state_init()
+state.transition('esrgan_verify')
 
 declare global {
   var __basedir: string
