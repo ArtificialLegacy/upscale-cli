ac07b1803756556eb8303afef37b881fe2ed7313
ArtificialLegacy <redsbro457@gmail.com>
2023-11-19 05:41:53 -0500

    organize state_init connections

--- src/modules/state/state_init.ts
+++ src/modules/state/state_init.ts
@@ -28,19 +28,26 @@ async function state_init(): Promise<StateMachine> {
   state
     .add_connection('esrgan_verify', 'esrgan_fail')
     .add_connection('esrgan_verify', 'esrgan_download')
-    .add_connection('esrgan_download', 'esrgan_fail')
     .add_connection('esrgan_verify', 'landing_menu')
+
+    .add_connection('esrgan_download', 'esrgan_fail')
     .add_connection('esrgan_download', 'landing_menu')
+
+    .add_connection('landing_menu', 'workload_menu')
     .add_connection('landing_menu', 'esrgan_manage')
+
     .add_connection('esrgan_manage', 'esrgan_fail')
     .add_connection('esrgan_manage', 'esrgan_download')
     .add_connection('esrgan_manage', 'landing_menu')
-    .add_connection('landing_menu', 'workload_menu')
-    .add_connection('workload_menu', 'landing_menu')
+
     .add_connection('workload_menu', 'esrgan_x4')
     .add_connection('workload_menu', 'esrgan_animex4')
+    .add_connection('workload_menu', 'landing_menu')
+
     .add_connection('esrgan_x4', 'workload_finish')
+
     .add_connection('esrgan_animex4', 'workload_finish')
+
     .add_connection('workload_finish', 'landing_menu')
 
   return state
