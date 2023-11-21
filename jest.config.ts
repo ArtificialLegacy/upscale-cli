b6488befe8d1cabfdbeeefca9a9862ab8b3640c0
ArtificialLegacy <redsbro457@gmail.com>
2023-11-21 10:29:42 -0500

    update configs to allow jest to work with inquirer

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,12 @@ import { pathsToModuleNameMapper } from 'ts-jest'
 import { compilerOptions } from './tsconfig.json'
 
 const config: Config = {
+  extensionsToTreatAsEsm: ['.ts'],
   verbose: true,
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true }],
   },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   modulePaths: ['<rootDir>/src/'],
